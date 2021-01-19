a7bad8c676586968e54c07a94e7484ccc684550f
chenwei <zhangchenwei115@163.com>
2021-01-19 12:21:34 +0100

    change

---
+++ pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/extract_poses.cpp" "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o"
+  "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/human_pose.cpp" "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o"
+  "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/peak.cpp" "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/src/peak.cpp.o"
+  "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/wrapper.cpp" "/home/chenwei/123/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/wrapper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../src"
+  "/usr/include/python3.6m"
+  "/usr/lib/python3/dist-packages/numpy/core/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_extractor.dir/wrapper.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/peak.cpp.o"
+  "../pose_extractor.pdb"
+  "../pose_extractor.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_extractor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
